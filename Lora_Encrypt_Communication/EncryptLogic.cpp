bd44c2fd754606e59ad0ee5a8128f8625f59a8cf
arad4228 <arad4228@gmail.com>
2022-02-14 21:42:58 +0900

    보안로직 제작(제작중) 업로드

--- Lora_Encrypt_Communication/EncryptLogic.cpp
+++ Lora_Encrypt_Communication/EncryptLogic.cpp
@@ -3,10 +3,10 @@
 #include <cstring>
 
 #include "BUILD/NUCLEO_F446RE/ARMC6/mbed_config.h"
+#include "cmsis_iccarm.h"
 #include "fhss_api.h"
 #include "mbedtls/sha256.h"
 #include "mbedtls/aes.h"
-#include "em_crrypo.h"
 
 using namespace std;
 
@@ -21,9 +21,10 @@ class Security{
         mbedtls_sha256_init(&AesKey);
         mbedtls_sha256_starts(&AesKey, 0);
         mbedtls_aes_init(&Ac);
+        this->rout = 1;
     }
 
-    uint32_t Read()
+    uint32_t Read(uint32_t value)
     {
         // SHA256에 넣을 값들을 buffer에 삽입.
         for(int i =0; i < 32 ; i++)
@@ -39,19 +40,27 @@ class Security{
         mbedtls_sha256_finish(&AesKey, Aesk);
         mbedtls_sha256_free(&AesKey);
 
-        
-
         // 암호화 부분의 ket를 256bi로 저장하고 값은 hash를 통해 구한 Akey를 사용한다.
         mbedtls_aes_setkey_enc(&Ac, Aesk, 256);
 
+        // IV를 설정하기.
+        for(int i = 7; i> 0; i--)
+            IV[i] = deveui[i];
+
+        // uint32 -> unsigned char로 변경하는 것으로 작성.
+        // 4byte to unsigned char 4개에 변경로직 작성이 필요.
+        
+        mbedtls_aes_crypt_cfb128(&Ac, MBEDTLS_AES_ENCRYPT, 32, IV, , );
         return Ci;
     }
 
     private:
+    int rout;
     unsigned char buffer[32];
     unsigned char Aesk[32];
     uint8_t nwkskey[16] = MBED_CONF_LORA_NWKSKEY;
     uint8_t appskey[16] = MBED_CONF_LORA_APPSKEY;
+    uint8_t deveui[7] = MBED_CONF_LORA_DEVICE_EUI;
     unsigned char IV[16];
     uint32_t Ci;
 };
